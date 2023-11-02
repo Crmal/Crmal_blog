e551925e3a71215c3c9123ffc3c69d6efa8bb825
Crmal <pjm2207@naver.com>
2023-11-02 17:08:06 +0900

    :sparkles: User service code 작성
    임시 마무리

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { AuthException, AuthExceptionType } from 'src/auth/exception';
 import { AuthService } from 'src/auth/service/auth.service';
-import { Repository } from 'typeorm';
+import { SignUpRequestDto } from 'src/auth/service/dto';
 
 import { User } from './entity/user.entity';
 import { UserFactory } from './factories/user.factory';
@@ -18,6 +18,7 @@ const mockUserFactory = {
 
 const mockUserRepository = {
   findOne: jest.fn(),
+  save: jest.fn(),
 };
 
 describe('UserService', () => {
@@ -34,6 +35,20 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UserService>(UserService);
+  });
+
+  it('회원가입 성공시 패스워드를 제외한 User를 응답한다.', async () => {
+    // given
+    const user = new SignUpRequestDto();
+    user.email = 'test@example.com';
+    user.password = 'testpassword';
+    mockUserFactory.createUser.mockReturnValue(new User(user.email, user.password));
+
+    // When
+    const result = await service.create(user);
+
+    // Then
+    expect(result).toHaveProperty('email', user.email);
   });
 
   it('회원 정보가 있을시 에러를 던진다.', async () => {
