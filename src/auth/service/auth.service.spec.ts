ccbc9b559ea007907a620416295d65fa0e4a2832
Crmal <pjm2207@naver.com>
2023-11-27 21:57:07 +0900

    :sparkles: AuthService testCode 작성 (Sign-in)
    sign-in 부분 testcode 작성하였습니다

--- src/auth/service/auth.service.spec.ts
+++ src/auth/service/auth.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entity/user.entity';
 import { UserService } from 'src/user/user.service';
 
 import { AuthException, AuthExceptionType } from '../exception';
@@ -22,14 +23,49 @@ describe('AuthService', () => {
     service = module.get<AuthService>(AuthService);
   });
 
-  it('해시화된 비밀번호를 만든다', async () => {
-    // Given
-    const password = 'testpassword';
+  describe('hashedPassword()', () => {
+    it('해시화된 비밀번호를 만든다', async () => {
+      // Given
+      const password = 'testpassword';
 
-    // When
-    const hashedPassword = await service.hashPassword(password);
+      // When
+      const hashedPassword = await service.hashPassword(password);
 
-    // Then
-    expect(bcrypt.compareSync(password, hashedPassword)).toBe(true);
+      // Then
+      expect(bcrypt.compareSync(password, hashedPassword)).toBe(true);
+    });
+  });
+
+  describe('signIn()', () => {
+    let signInRequest: SignInRequest;
+    beforeEach(() => {
+      signInRequest = {
+        email: 'test@example.com',
+        password: 'testpassword',
+      };
+    });
+    it('로그인 성공', async () => {
+      // Given
+      mockUserService.findOneByEmail.mockResolvedValue(
+        new User(signInRequest.email, signInRequest.password),
+      );
+
+      // When
+      const accessToken = await service.signIn(signInRequest);
+
+      // Then
+      expect(accessToken).toEqual(expect.any(String));
+    });
+
+    it('회원이 없을시 에러', async () => {
+      // Given
+      const expectedError = new AuthException(AuthExceptionType.NOT_FOUND_USER);
+      mockUserService.findOneByEmail.mockRejectedValue(expectedError);
+
+      // When
+
+      // then
+      expect(service.signIn(signInRequest)).rejects.toThrow(expectedError);
+    });
   });
 });
