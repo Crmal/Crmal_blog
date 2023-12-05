393eafdec7007d276f644ea820d613a9a0a10cd6
Crmal <pjm2207@naver.com>
2023-12-05 21:50:06 +0900

    :sparkles: Response Interceptor 추가

--- src/common/interceptor/transform.interceptor.ts
+++ src/common/interceptor/transform.interceptor.ts
@@ -4,11 +4,41 @@ import { map } from 'rxjs/operators';
 
 export interface Response<T> {
   data: T;
+  [key: string]: any;
 }
 
+const jsonFilter = ['link', 'metadata'];
+
 @Injectable()
-export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+export class TransformInterceptor<T = any> implements NestInterceptor<T, Response<T>> {
+  private isObject = (data: any): data is Record<string, any> =>
+    typeof data === 'object' && data !== null;
+
+  private handleResponse = (data: T): Record<string, any> => {
+    const res: Record<string, any> = { data: {} };
+
+    Object.keys(data).forEach(key => {
+      if (jsonFilter.includes(key)) {
+        res[key] = data[key];
+      } else {
+        res.data[key] = data[key];
+      }
+    });
+
+    return res;
+  };
+
+  private handleObjectData = (data: T): Response<T> => {
+    const res = this.handleResponse(data);
+    return res as Response<T>;
+  };
+
+  private handleNonObjectData = (data: T): Response<T> => ({ data });
+
+  private handleData = (data: T): Response<T> =>
+    this.isObject(data) ? this.handleObjectData(data) : this.handleNonObjectData(data);
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
-    return next.handle().pipe(map(data => ({ data })));
+    return next.handle().pipe(map(this.handleData));
   }
 }
