8a67dffb006c8e21f2e3bdf69a4fe6498f39026d
Crmal <pjm2207@naver.com>
2024-03-16 01:38:18 +0900

    :sparkles: Auth Grard 및 jwtStartegy 추가

---
+++ src/auth/guard/jwt.startegy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { OrNeverType } from 'src/util/types/or-never.type';
+
+import { AuthException, AuthExceptionType } from '../exception';
+
+import { JwtPayloadType } from './types/jwt-payload.type';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([request => request?.cookies?.accessToken]),
+      secretOrKey: configService.get<string>('JWT_ACCESS_TOKEN_SECRET_KEY'), // JWT secret key
+    });
+  }
+
+  public validate(payload: JwtPayloadType): OrNeverType<JwtPayloadType> {
+    if (!payload.id) {
+      throw new AuthException(AuthExceptionType.CONFLICT_DUPLICATE_USER);
+    }
+
+    return payload;
+  }
+}
