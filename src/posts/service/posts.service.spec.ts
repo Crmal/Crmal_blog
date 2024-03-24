83e03872e628a5643636f29638287bddc2f62af8
Crmal <pjm2207@naver.com>
2024-03-24 23:30:55 +0900

    :sparkles: Post create testCode 작성
    연습용 추후 수정예정

--- src/posts/service/posts.service.spec.ts
+++ src/posts/service/posts.service.spec.ts
@@ -1,13 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserService } from 'src/user/user.service';
+import { Repository } from 'typeorm';
 
+import { Post } from '../entity/post.entity';
+
+import { PostFactory } from './Factory/post.factory';
+import { PostCreateRequest } from './dto/postCreateRequest.dto';
 import { PostsService } from './posts.service';
+
+const mockPostRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+};
+
+const mockPostFactory = {
+  create: jest.fn(),
+};
+
+const mockUserService = {
+  findOneById: jest.fn(),
+};
 
 describe('PostsService', () => {
   let service: PostsService;
+  let postRepository: Repository<Post>;
+  let postFactory: PostFactory;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        { provide: getRepositoryToken(Post), useValue: mockPostRepository },
+        { provide: PostFactory, useValue: mockPostFactory },
+        { provide: UserService, useValue: mockUserService },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
@@ -19,5 +48,20 @@ describe('PostsService', () => {
 
   describe('create()', () => {
     const userId = '1';
+    const postCreateRequest: PostCreateRequest = {
+      title: 'testTitle',
+      description: 'testDescription',
+    };
+    it('should create a post', async () => {
+      // Given
+      mockUserService.findOneById.mockReturnValue(userId);
+      mockPostFactory.create.mockReturnValue({ id: 1, ...postCreateRequest });
+      mockPostRepository.save.mockReturnValue(postCreateRequest);
+      // When
+      const result = await service.create(postCreateRequest, userId);
+
+      // Then
+      expect(result).toEqual(expect.objectContaining(postCreateRequest));
+    });
   });
 });
